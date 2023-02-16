405963a0f1a3f52f4ef4b43d738c51ad834b17f5
ZhengLiu97 <zheng.liu97@outlook.com>
2023-02-17 02:00:23 +0800

    add setup.py for this library

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
+import os.path as op
 from setuptools import setup 
 from pathlib import Path
 
 version = None
-with open(Path(rf"gad4fun\version.py")) as f:
+with open(Path(rf"gadpipe\version.py")) as f:
     version = f.read().split('=')[1].strip().strip('\'')
 if version is None:
     raise RuntimeError('Could not determine version')
 
 setup(
-    name='gad4fun',
+    name='gadpipe',
     author='ADEPT',
     license='MIT',
     version=version,
-    description='reconstruction pipelines based on gadgetron',
+    description='reconstruction pipelines based on gadgetron framework',
     include_package_data=True,
     packages=[
-        'gad4fun',
-        'gad4fun/utils',
-        'gad4fun/recon',
+        'gadpipe',
+        'gadpipe/utils',
+        'gadpipe/recon',
+        'gadpipe/tests',
         ],
     package_data={
-        "gad4fun/recon_config": ["*.xml"] ,  # all the config files(xml)
+        "gadpipe": [
+            op.join("recon_config", "*.xml"), # all the config files(xml)
+            ],  
         },
     install_requires=[
         'numpy',
@@ -29,6 +33,6 @@ setup(
         'matplotlib'
         ],
     project_urls={
-        'Source': 'https://github.com/medlab/gadgetron-4-fun/',
+        'Source': 'https://github.com/medlab/gadgetron-4-fun',
         },
 )
