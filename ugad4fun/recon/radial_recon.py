4a932a4d6b18e7c8282ce256f096d32bd4e73a17
ZhengLiu97 <zheng.liu97@outlook.com>
2023-02-17 14:40:09 +0800

    rename back to recon

--- ugad4fun/recon/radial_recon.py
+++ ugad4fun/recon/radial_recon.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from pynufft import NUFFT
-from ugadget.utils.dummy_conn import DummyConn
+from ugad4fun.utils.dummy_conn import DummyConn
 
 
 def accumulate_acquisitions(acquisitions, header):
