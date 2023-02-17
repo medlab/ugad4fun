c3d7d0c3e51c85f3d2efa5ace93422c5a8811cb1
ZhengLiu97 <zheng.liu97@outlook.com>
2023-02-17 10:45:59 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,27 +3,27 @@ from setuptools import setup
 from pathlib import Path
 
 version = None
-with open(Path(rf"gadpipe\version.py")) as f:
+with open(Path(rf"ugadget\version.py")) as f:
     version = f.read().split('=')[1].strip().strip('\'')
 if version is None:
     raise RuntimeError('Could not determine version')
 
 setup(
-    name='gadpipe',
+    name='ugadget',
     author='ADEPT',
     license='MIT',
     version=version,
     description='reconstruction pipelines based on gadgetron framework',
     include_package_data=True,
     packages=[
-        'gadpipe',
-        'gadpipe/utils',
-        'gadpipe/recon',
-        'gadpipe/tests',
+        'ugadget',
+        'ugadget/utils',
+        'ugadget/workflow',
+        'ugadget/tests',
         ],
     package_data={
-        "gadpipe": [
-            op.join("recon_config", "*.xml"), # all the config files(xml)
+        "ugadget": [
+            op.join("workflow_config", "*.xml"), # all the config files(xml)
             ],  
         },
     install_requires=[
@@ -33,6 +33,6 @@ setup(
         'matplotlib'
         ],
     project_urls={
-        'Source': 'https://github.com/medlab/gadgetron-4-fun',
+        'Source': 'https://github.com/medlab/ugadget',
         },
 )
