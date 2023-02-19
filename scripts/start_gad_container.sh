470585bb0b5dd69f36e0e02056679444a3c02a6a
C-- <51979184+Nothing10086@users.noreply.github.com>
2023-02-19 17:41:55 +0800

    Update start_gad_container.sh

--- scripts/start_gad_container.sh
+++ scripts/start_gad_container.sh
@@ -5,4 +5,6 @@ sudo docker run \
 --publish=9080:9080 \
 --publish=8002:8002  \
 --publish=9001:9001 \
+--volume=$(pwd)/../ugad4fun/recon_config:/usr/local/lib/python3.8/dist-packages/ugad4fun/recon_config \
+--volume=$(pwd)/../ugad4fun/recon:/usr/local/lib/python3.8/dist-packages/ugad4fun/recon \
 --rm -it gadgetron_uih:v1 /usr/local/bin/gadgetron
